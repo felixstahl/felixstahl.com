fd3aa58e9bbb56ffc207f7c56e9c89eb4a7ace3b
Felix <31844847+felixstahl@users.noreply.github.com>
2019-02-04 08:17:45 +0100

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Felix Ståhl</title>
+<!-- custom font -->
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<!-- reset css stylesheet -->
+<link rel="stylesheet" type="text/css" href="/static/css/reset.css">
+<!-- css stylesheet  -->
+<link rel="stylesheet" type="text/css" href="/static/css/stylesheet.css">
+</head>
+
+<body>
+<div class="outerWrapper">
+	<div class="innerWrapper">
+		<h1 class="welcome">Welcome to my sandbox!</h1>
+		<div class="welcome">
+				<img class="me" alt="Picture of me" src="/static/pictures/me.png"/>
+				<a href="https://www.kth.se"><img class="KTH" alt="KTH logo" src="/static/pictures/kth.png"/></a>
+		</div>
+		<div class="description">
+			<p>My name is Felix Ståhl and I'm a 22 year old from Sweden. I'm also a bachelor student in computer engineering
+				at KTH Royal Institute of Technology.</p>
+				<p>This website is always a work in progress and as the title implies, it's supposed to be where I experiment with stuff, but also where you can find information about me.</p>
+		</div>
+
+		<hr class="indexSeparator">
+
+		<div class="navBar">
+				<a href="https://www.linkedin.com/in/felix-st%C3%A5hl-975746171/"><img class="navImage" alt="LinkedIn" src="/static/pictures/linkedin.png"/></a>
+				<a href="https://github.com/felixstahl"><img class="navImage" alt="Github" src="/static/pictures/github.png"/></a>
+				<a href="mailto:me@felixstahl.com"><img class="navImage" alt="Email" src="/static/pictures/email.png"/></a>
+		</div>
+	</div>
+</div>
+<footer>
+	<p>This website is created by me. I do not own any of the images (except the one on me)</p>
+</footer>
+
+</body>
+</html>
